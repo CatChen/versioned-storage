31279670749d56c73be5737a00c78af910476463
Cat Chen <catchen@catchen.me>
2019-11-27 11:07:05 -0800

    Changed Storage.ts back to declaration only

--- src/Storage.ts
+++ src/Storage.ts
@@ -1,8 +1,8 @@
-export default class Storage<T> {
-  constructor(name: string, version: number) {}
-  read(): T | null {
-    return null;
-  }
-  write(value: T): void {}
-  static reset(): void {}
+declare class Storage<T> {
+  constructor(name: string, version: number);
+  read(): T | null;
+  write(value: T): void;
+  static reset(): void;
 }
+
+export default Storage;
